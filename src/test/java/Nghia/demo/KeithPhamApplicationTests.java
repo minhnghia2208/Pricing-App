b63f9718d5c1e1435194657a3142846c180b231f
Keith Pham <minhnghia2208@gmail.com>
2023-02-26 19:05:08 -0500

    init proj

---
+++ src/test/java/Nghia/demo/KeithPhamApplicationTests.java
@@ -0,0 +1,13 @@
+package Nghia.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeithPhamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
